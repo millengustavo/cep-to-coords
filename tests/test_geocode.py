a1f66eb8189a49e6b289927c435dde19f6b64a8c
funcional <funcional@HEAL0013.funcional.local>
2019-11-11 17:18:30 -0300

    Created tests for the other functions

--- tests/test_geocode.py
+++ tests/test_geocode.py
@@ -1,8 +1,36 @@
 import unittest
-from src.geocode_python.geocode import cep_to_coords
+from src.geocode_python.geocode import cep_to_coords, address_from_cep, geocode
 
 
-class TestCEPtoCoords(unittest.TestCase):
+class TestGeocode(unittest.TestCase):
+
+    def test_address_from_cep(self):
+        d = {'01310-200': 'Avenida Paulista São Paulo Brasil',
+             '22021001': 'Avenida Atlântica Rio de Janeiro Brasil',
+             '': '-',
+             '123456': '-'}
+
+        self.assertEqual(
+            d['01310-200'], address_from_cep('01310-200'), "Wrong result!")
+        self.assertEqual(
+            d['22021001'], address_from_cep('22021001'), "Wrong result!")
+        self.assertEqual(
+            d[''], address_from_cep(''), "Wrong result!")
+        self.assertEqual(
+            d['123456'], address_from_cep('123456'), "Wrong result!")
+
+    def test_geocode(self):
+        # Defining the values as tuples because lists are unhashable
+        d = {'Avenida Paulista São Paulo Brasil': (-46.65596048644724, -23.56150875),
+             'Avenida Atlântica Rio de Janeiro Brasil': (-43.189305, -22.984457),
+             '-': (float('nan'), float('nan'))}
+
+        self.assertEqual(
+            d['Avenida Paulista São Paulo Brasil'], tuple(geocode('Avenida Paulista São Paulo Brasil')), "Wrong result!")
+        self.assertEqual(
+            d['Avenida Atlântica Rio de Janeiro Brasil'], tuple(geocode('Avenida Atlântica Rio de Janeiro Brasil')), "Wrong result!")
+        self.assertEqual(
+            str(d['-']), str(tuple(geocode('-'))), "Wrong result!")
 
     def test_cep_to_coords(self):
         # Defining the values as tuples because lists are unhashable
