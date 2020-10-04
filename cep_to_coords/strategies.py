1317dc93cd5839ba9d77d385b3e40ac59706c3ac
Gustavo Millen <millengustavo@gmail.com>
2020-10-04 10:57:36 -0300

    Remove pycep correios dependency

--- cep_to_coords/strategies.py
+++ cep_to_coords/strategies.py
@@ -1,6 +1,7 @@
 import os
 import requests
-import pycep_correios
+
+from .viacep import get_address_from_cep
 from .base import CEPConverter, Coordinates
 
 
@@ -17,8 +18,16 @@ class CEPAbertoConverter(CEPConverter):
 class CorreiosPhotonCoordinates(Coordinates):
     def fetch_address(self):
         try:
-            search_result = pycep_correios.consultar_cep(self.clean_cep)
-            address = " ".join([search_result["end"], search_result["cidade"], "Brasil"])
+            search_result = get_address_from_cep(self.clean_cep)
+            address = " ".join(
+                [
+                    search_result["logradouro"],
+                    search_result["bairro"],
+                    search_result["cidade"],
+                    search_result["uf"],
+                    "Brasil",
+                ]
+            )
             # Treating the case when Correios API return an empty json
             if address == " Brasil":
                 address = "-"
@@ -33,15 +42,14 @@ class CorreiosPhotonCoordinates(Coordinates):
                 print("Não foi possível converter, retornando NaNs...")
                 return {"latitude": float("nan"), "longitude": float("nan")}
 
-            r = requests.get("".join(["http://photon.komoot.de/api?q=", address, "&limit=1"]))
+            r = requests.get(
+                "".join(["http://photon.komoot.de/api?q=", address, "&limit=1"])
+            )
 
             result = r.json()
             # list with lon, lat
             coordinates = result["features"][0]["geometry"]["coordinates"]
-            return {
-                "latitude": coordinates[1],
-                "longitude": coordinates[0]
-            }
+            return {"latitude": coordinates[1], "longitude": coordinates[0]}
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
 
@@ -57,13 +65,13 @@ class CEPAbertoCoordinates(Coordinates):
             url = f"https://www.cepaberto.com/api/v3/cep?cep={self.clean_cep}"
             # Sign up for your free token on: https://cepaberto.com/
             # export CEP_ABERTO_TOKEN='your-token'
-            headers = {'Authorization': f'Token token={os.getenv("CEP_ABERTO_TOKEN")}'}
+            headers = {"Authorization": f'Token token={os.getenv("CEP_ABERTO_TOKEN")}'}
             response = requests.get(url, headers=headers)
             json_response = response.json()
             return {
                 "latitude": float(json_response["latitude"]),
                 "longitude": float(json_response["longitude"]),
-            } 
+            }
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
 
