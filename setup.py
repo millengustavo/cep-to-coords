50eb8c0070395c69d4f715dc4e1e0b948b280ac5
Gustavo Millen <millengustavo@gmail.com>
2021-03-13 13:27:19 -0300

    Change version to 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cep_to_coords",
-    version="0.0.1",
+    version="0.1.0",
     author="Gustavo Millen",
     author_email="millengustavo@gmail.com",
     description="Convert CEP (Brazilian zip code) to coordinates",
